86ea1cb0464ad35c1822aaccda5b8a8321d4e53f
zeeshan666 <zeeshanmanzoor.zm@gmail.com>
2019-09-08 13:52:46 +0500

    Updates

---
+++ precache-manifest.aeb1a5b595baf5c6a405afaa197da02f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55e0a3ae756315751cf376175541331",
+    "url": "/full-stack-react-app/index.html"
+  },
+  {
+    "revision": "e2a10fb139d6f7f574ea",
+    "url": "/full-stack-react-app/static/css/2.8398c87b.chunk.css"
+  },
+  {
+    "revision": "6d26140bc15800682574",
+    "url": "/full-stack-react-app/static/css/main.de07c20b.chunk.css"
+  },
+  {
+    "revision": "e2a10fb139d6f7f574ea",
+    "url": "/full-stack-react-app/static/js/2.284da84a.chunk.js"
+  },
+  {
+    "revision": "6d26140bc15800682574",
+    "url": "/full-stack-react-app/static/js/main.30e0886d.chunk.js"
+  },
+  {
+    "revision": "2456c43d4f788b1a7532",
+    "url": "/full-stack-react-app/static/js/runtime~main.3cacfd74.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/full-stack-react-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/full-stack-react-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/full-stack-react-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/full-stack-react-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/full-stack-react-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
